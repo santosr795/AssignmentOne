80dd19206bf5d62a7f5aed635f6f7e9b42595f57
santosr795 <36488171+santosr795@users.noreply.github.com>
2018-09-06 18:58:18 -0400

    Add files via upload

---
+++ bottle.java
@@ -0,0 +1,20 @@
+package bottle;
+import java.util.Scanner; 
+
+public class bottle {
+private int pennies; 
+Scanner keyboard = new Scanner(System.in);
+static final int MIN = 0;
+ static void MAX() {
+	 int MAX = 500; 
+	 return; 
+ }
+ public void read(){
+	int read = keyboard.nextInt(); 	
+	
+ }
+ public void add() {
+	 
+ }
+
+}//End class
