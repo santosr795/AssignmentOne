0a37ccf72a4d355876df697e1bd83ee41b5530cf
santosr795 <36488171+santosr795@users.noreply.github.com>
2018-09-21 21:51:06 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,8 +1,34 @@
+import java.util.*;
 public class Student {
 	private String firstName;
 	private String lastName; 
-	private String Address; 
+	private String address; 
 	private String phoneNumber; 
+	Scanner keyboard = new Scanner(System.in); 
 	
+	public void readInput() {
+		System.out.println("Enter Name");
+		firstName = keyboard.nextLine(); 
+		System.out.println("Enter Last Name");
+		lastName = keyboard.nextLine(); 
+		System.out.println("Enter address."); 
+		address = keyboard.nextLine(); 
+		System.out.println("Enter Phone Number");
+		phoneNumber = keyboard.nextLine(); 
+				
+	}
+	
+	
+	
+	public void  writeOutput() {
+	System.out.print("Student Name " + firstName +" "+ lastName);
+	System.out.print("Address " + address);
+	System.out.print("Phone Number " + phoneNumber);
+		
+	}
+	public void calculateData() {
+		
+		
+	}
 
 }
