4c2a25c5d35d05ab95040bebf4c67c904836c486
Ronald Santos <36488171+santosr795@users.noreply.github.com>
2018-09-26 00:25:37 -0400

    Update University.java

--- University.java
+++ University.java
@@ -1,25 +1,49 @@
 import java.util.Scanner; 
 
+@SuppressWarnings("unused")
 public class University {
-	Scanner keyboard = new Scanner(System.in); 
+	//University totalTution = new University();  
+	private double totalTution = 0; 
+	private String inOutStateTution; 
+	private int inState = 0;
+	private int outState = 0;
+	private double lateFee; 
+	private double healthCare;
+	private double incidentalFee;
+	private int numberOfStudent = 0; 
+	private double mealPlan;
+	OutPut outPut = new OutPut(); 
 	
-	private String creditEnrolled; 
-	private String inStateRate; 
-	private String lateFee; 
-	private String onCampusFood; 
-	private String healthCare; 
 	
-
-	private void Calculation() {
-		
-	}
 	
+	Student person = new Student(); 
+	Scanner keyboard = new Scanner(System.in);
 	public void printDataForSchoolReport() {
-		
-	}
-	public void collectDataForReport(Student student) {
-		
-	}
+		System.out.println("Tution " + totalTution); 
+		System.out.println("Number of Students " + numberOfStudent); 
+		System.out.println("InState " + inState); 
+		System.out.println("OutState " + outState); 
+		//System.out.println(x); 
+		//System.out.println(x); 
 
+		
+	}
+	public void collectDataForReport(Student person) {
+		numberOfStudent = numberOfStudent + 1; 
+		totalTution =  totalTution + person.getTution();
+		lateFee = lateFee + person.getLateFee(); 
+		mealPlan = mealPlan + person.getMealPlan();
+		healthCare = healthCare + person.getHealthCare();
+		incidentalFee = incidentalFee + person.getHealthCare();
+		inOutStateTution = person.getInOutState(); 
+		if(inOutStateTution.equalsIgnoreCase("Yes")) {
+			inState = inState + 1; 
+			
+		}
+		else {
+			outState = outState + 1; 
+		}
+		
+}
 
 }
