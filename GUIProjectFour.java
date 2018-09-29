14f664dee57c670e484be851aa4dfef14d2a32e1
Ronald Santos <36488171+santosr795@users.noreply.github.com>
2018-09-28 23:02:24 -0400

    Add files via upload

---
+++ GUIProjectFour.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GUIProjectFour {
+	JPanel titlePanel = new JPanel();
+	JPanel questionPanel = new JPanel();
+	JPanel inputNumberPanel = new JPanel();
+	JPanel addAndSubtractButtonPanel = new JPanel();
+	JPanel answerPanel = new JPanel();
+	JPanel nextNumberPanel = new JPanel();
+	
+	JPanel tittleLabel = new JPanel(); 
+	JPanel inputLabel = new JPanel(); 
+	
+}
