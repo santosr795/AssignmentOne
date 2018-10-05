1191c3386c0fb2e0c6b61c9c3c4d3171cc5916ae
Ronald Santos <36488171+santosr795@users.noreply.github.com>
2018-10-04 22:37:55 -0400

    Update findingRoots.java

--- findingRoots.java
+++ findingRoots.java
@@ -20,22 +20,28 @@ public class findingRoots {
 		this.coefficient1 = 0; 
 		this.constant = 0; 
 	}
-	findingRoots(double coeff5, double coeff4){
+	findingRoots(double coeff5, double coeff4, double Constant){
 		this.leadingCoefficient = coeff5; 
 		this.coefficient4 = coeff4; 
 		//this.coefficient3 = coeff3; 
 		//this.coefficient2 = coeff2; 
 		//this.coefficient1 = coeff1; 
-		//this.constant = Constant;
+		this.constant = Constant;
 		//double coeff4, double coeff3, double coeff2, double coeff1, double Constant)
 	}
 	private double scaleCalculation(double upper_Bound, double lower_Bound) {
 		upper_Bound = this.upperBound;
 		lower_Bound = this.lowerBound;
 		double root = 0; 
+		findingRoots tester = new findingRoots();  
 		for(int i = 1; i >= upperXScale; i++ ) {
 			
 			lowerXScale = lowerXScale + i;
+			root = ( this.leadingCoefficient*(lowerXScale)+ this.coefficient4 *(lowerXScale) + this.constant);
+			if(root == 0) {
+				
+			}
+			
 		}
 		
 		
