250ce6ef9ac9022cd5a3cccd64be861de891b273
Ronald Santos <36488171+santosr795@users.noreply.github.com>
2018-11-16 21:42:36 -0500

    Update FindingPrime.java

--- FindingPrime.java
+++ FindingPrime.java
@@ -7,30 +7,43 @@ public class PrimeNumbers {
 		ArrayList <Double> factors = new ArrayList<Double>();
 		System.out.println("Enter a number that you to find out if it is a prime Number.");
 		double input  = keyboard.nextDouble(); 
-		double resultSqrt = Math.sqrt(input);
-		int factorIndex = 0; 
-		System.out.println("Result SquareRoot is equals to " + resultSqrt);
-		for(double index = 5; index <= resultSqrt; index = index + 2) {
-			double answer = input/ index; 
-			System.out.println("answer is equals to " + answer);
-			//This line will check if dividing input by index will have a reminder
-			// equals to zero. if it does that means is a factor
-			if(input%index == 0 )
-				//In here resultSquare will change by resultS square rooting Answer; 
+		double squareRootInput = Math.sqrt(input);
+		squareRootInput = Math.round(squareRootInput);
+		System.out.println("Square root is Equals to " + squareRootInput);
+		for(double factorials = 2; factorials <= squareRootInput; factorials++) {
+			double answerDivision = input / factorials; 
+			/*
+			 * The following If Statement will check if 
+			 * input divide factorial has decimal point equals 
+			 * to Zero.
+			 * 
+			 */
+			if( input%factorials == 0 ) {
+				/*
+				 * It will printOut the factorials as its a factor.
+				 * squareRootInput will change by answerDivision being 
+				 * square root it. squareRootInput will become a smaller number
+				 * than before.
+				 * At the End the squareRootInput will be round the nears number. 
+				 * As it will be easier to calculate. 
+				 */
+					System.out.println(factorials);
+					squareRootInput = Math.sqrt(answerDivision);
+					squareRootInput = Math.round(squareRootInput);
 				
-				for(factorIndex < 1; factorIndex++) {
-					//In here you will create the ArrayList 
-					// 
-				}
 			}
-		if(resultSqrt % 2 == 0) {
+			/*
+			 * after running all throught the scale from 2 to value 
+			 * of squareRootInput the program will printout that the 
+			 * number is not a prime number.
+			 */
+			else if(factorials >= squareRootInput) {
+				System.out.println("Not a Prime number");
+				
+			}
+		
+			
 			
 		}
-		if (resultSqrt % 3 == 0 ) {
-			
-		}
-		} 
-
 	}
-	
 }
