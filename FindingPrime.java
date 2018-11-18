ba8d72d05ebce68e5dff590e77a7ecbb21d4a60b
Ronald Santos <36488171+santosr795@users.noreply.github.com>
2018-11-17 20:21:21 -0500

    Update FindingPrime.java

--- FindingPrime.java
+++ FindingPrime.java
@@ -4,21 +4,23 @@ public class PrimeNumbers {
 	
 	public static void main(String []args) {
 		Scanner keyboard = new Scanner(System.in); 
+		int arrayIndex = 0; 
 		ArrayList <Double> factors = new ArrayList<Double>();
 		System.out.println("Enter a number that you to find out if it is a prime Number.");
 		double input  = keyboard.nextDouble(); 
 		double squareRootInput = Math.sqrt(input);
 		squareRootInput = Math.round(squareRootInput);
 		System.out.println("Square root is Equals to " + squareRootInput);
-		for(double factorials = 2; factorials <= squareRootInput; factorials++) {
-			double answerDivision = input / factorials; 
+		for(double possibleFactorS = 2; possibleFactorS <= squareRootInput; possibleFactorS++) {
+			double answerDivision = input / possibleFactorS; 
+			System.out.println("answerDivision is equals to " + answerDivision + " Plus Factorials " + possibleFactorS );
 			/*
 			 * The following If Statement will check if 
 			 * input divide factorial has decimal point equals 
 			 * to Zero.
 			 * 
 			 */
-			if( input%factorials == 0 ) {
+			if( input%possibleFactorS == 0 ) {
 				/*
 				 * It will printOut the factorials as its a factor.
 				 * squareRootInput will change by answerDivision being 
@@ -27,23 +29,43 @@ public class PrimeNumbers {
 				 * At the End the squareRootInput will be round the nears number. 
 				 * As it will be easier to calculate. 
 				 */
-					System.out.println(factorials);
-					squareRootInput = Math.sqrt(answerDivision);
-					squareRootInput = Math.round(squareRootInput);
-				
+				System.out.println("Square Root " + squareRootInput);
+				//squareRootInput = Math.sqrt(answerDivision);
+
+				//squareRootInput = Math.round(squareRootInput);
+				//int i = 1; 
+				//for(int o = 0; arrayIndex < i;arrayIndex++) {		
+						factors.add(arrayIndex, possibleFactorS);
+						System.out.println("This is the array list values " + factors.get(arrayIndex));
+						arrayIndex++; 
+						
+					/*System.out.println(factorials);
+					for(int m = 0;m <= factors.size(); m++) {
+						
+					}
+				}
+				i = i + 1;
+				*/
 			}
 			/*
 			 * after running all throught the scale from 2 to value 
 			 * of squareRootInput the program will printout that the 
 			 * number is not a prime number.
 			 */
-			else if(factorials >= squareRootInput) {
+			else if(possibleFactorS >= squareRootInput) {
 				System.out.println("Not a Prime number");
 				
 			}
 		
+		
 			
 			
+		}
+		
+		
+		for(int i = 0; i < factors.size(); i++) {
+			System.out.println("This is the array list values " + factors.get(i));
+	
 		}
 	}
 }
